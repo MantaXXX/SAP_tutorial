1d61fef327c407abebfee4387e13a20e17c144a1
Jia Lu <pochialu@gmail.com>
2021-09-23 17:29:19 +0800

    feat: add function to create fragment inside view

--- controller/HelloPanel.controller.js
+++ controller/HelloPanel.controller.js
@@ -1,6 +1,11 @@
 sap.ui.define(
-  ["sap/ui/core/mvc/Controller", "sap/m/MessageToast"],
-  function (Controller, MessageToast) {
+  [
+    "sap/ui/core/mvc/Controller",
+    "sap/m/MessageToast",
+    // use Fragment module to load framgent
+    "sap/ui/core/Fragment",
+  ],
+  function (Controller, MessageToas, Fragment) {
     "use strict";
     return Controller.extend("sap.ui.demo.walkthrough.controller.HelloPanel", {
       onShowHello: function () {
@@ -12,6 +17,25 @@ sap.ui.define(
         var sMsg = oBundle.getText("helloMsg", [sRecipient]);
         // show message
         MessageToast.show(sMsg);
+      },
+      onOpenDialog: function () {
+        var oView = this.getView();
+        // console.log(oView);
+
+        // create dialog lazily
+        if (!this.pDialog) {
+          this.pDialog = Fragment.load({
+            id: oView.getId(),
+            name: "sap.ui.demo.walkthrough.view.HelloDialog",
+          }).then((oDialog) => {
+            // connect dialog to the root view of this component (models, lifecycle)
+            oView.addDependent(oDialog);
+            return oDialog;
+          });
+        }
+        this.pDialog.then((oDialog) => {
+          oDialog.open();
+        });
       },
     });
   }
