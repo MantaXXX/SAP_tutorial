4fc3501a8b4ff11aebd78e6f59323278e5b1fa97
Jia Lu <pochialu@gmail.com>
2021-09-23 14:52:42 +0800

    feat: create JSON model for data binding

--- controller/App.controller.js
+++ controller/App.controller.js
@@ -1,9 +1,27 @@
 // controller.js as same as <script> in Vue's component to manage page's interaction
 sap.ui.define(
-  ["sap/ui/core/mvc/Controller", "sap/m/MessageToast"],
-  function (Controller, MessageToast) {
+  [
+    "sap/ui/core/mvc/Controller",
+    "sap/m/MessageToast",
+    "sap/ui/model/json/JSONModel",
+  ],
+  // pass module objects as parameters in function
+  function (Controller, MessageToast, JSONModel) {
     "use strict";
     return Controller.extend("sap.ui.demo.walkthrough.controller.App", {
+      // onInit, a lifecycle method as same as Vue
+      onInit: function () {
+        // set data for view, as same as the variables in Vue's component for data binding
+        var oData = {
+          recipient: {
+            name: "World",
+          },
+        };
+        // set data as JSON model on view
+        var oModel = new JSONModel(oData);
+        // setModel() function makes this model be used within XML view
+        this.getView().setModel(oModel);
+      },
       onShowHello: function () {
         MessageToast.show("Hello World");
       },
