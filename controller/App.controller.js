b3e5a4a853b65e055685ff3daf7861a88c870012
Jia Lu <pochialu@gmail.com>
2021-09-23 14:27:11 +0800

    feat: use modules from SAPUI5 library

--- controller/App.controller.js
+++ controller/App.controller.js
@@ -1,10 +1,12 @@
 // controller.js as same as <script> in Vue's component to manage page's interaction
-sap.ui.define(["sap/ui/core/mvc/Controller"], function (Controller) {
-  "use strict";
-  return Controller.extend("sap.ui.demo.walkthrough.controller.App", {
-    onShowHello: function () {
-      // show a native JavaScript alert
-      alert("Hello World");
-    },
-  });
-});
+sap.ui.define(
+  ["sap/ui/core/mvc/Controller", "sap/m/MessageToast"],
+  function (Controller, MessageToast) {
+    "use strict";
+    return Controller.extend("sap.ui.demo.walkthrough.controller.App", {
+      onShowHello: function () {
+        MessageToast.show("Hello World");
+      },
+    });
+  }
+);
