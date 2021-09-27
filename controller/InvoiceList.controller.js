f435438268f9a24b7a653fc85c896c582403e100
Jia Lu <pochialu@gmail.com>
2021-09-27 10:33:13 +0800

    feat" add SAPUI5 filter function in search field

--- controller/InvoiceList.controller.js
+++ controller/InvoiceList.controller.js
@@ -3,8 +3,13 @@ sap.ui.define(
     "sap/ui/core/mvc/Controller",
     "sap/ui/model/json/JSONModel",
     "../model/formatter",
+    // loading two dependencies for filtering:
+    // Filter object hold the configuration for filter action
+    // FilterOperator is a helper type to specify the Filter
+    "sap/ui/model/Filter",
+    "sap/ui/model/FilterOperator",
   ],
-  function (Controller, JSONModel, formatter) {
+  function (Controller, JSONModel, formatter, Filter, FilterOperator) {
     "use strict";
 
     return Controller.extend("sap.ui.demo.walkthrough.controller.InvoiceList", {
@@ -14,6 +19,26 @@ sap.ui.define(
           currency: "EUR",
         });
         this.getView().setModel(oViewModel, "view");
+      },
+      onFilterInvoices: function (oEvent) {
+        // build filter array
+        var aFilter = [];
+        // getParameter("query") to get the content in the search field
+        var sQuery = oEvent.getParameter("query");
+        // search in the "ProductName" path
+        // Specify a filter operator that will search for the given query string
+        if (sQuery) {
+          aFilter.push(
+            new Filter("ProductName", FilterOperator.Contains, sQuery)
+          );
+        }
+
+        // filter binding
+        // Asking the view for list with the helper byID()
+        var oList = this.byId("invoiceList");
+        // Access the binding of the aggregation items to filter with newly constructed filter object
+        var oBinding = oList.getBinding("items");
+        oBinding.filter(aFilter);
       },
     });
   }
