683f8eda83394e2b83ce902b6e67b36a7fafce85
Jia Lu <pochialu@gmail.com>
2021-09-29 17:09:03 +0800

    feat: Add path back to the parent page or history

--- controller/Detail.controller.js
+++ controller/Detail.controller.js
@@ -1,28 +1,48 @@
-sap.ui.define(["sap/ui/core/mvc/Controller"], function (Controller) {
-  "use strict";
-  return Controller.extend("sap.ui.demo.walkthrough.controller.Detail", {
-    onInit: function () {
-      // fetch the app router instance
-      var oRouter = this.getOwnerComponent().getRouter();
-      // getRoute() method attach to the "detail" route
-      // attachPatternMatched() method register an internal callback "_onObjectMatched" which will be executed when the route is hit
-      oRouter
-        .getRoute("detail")
-        .attachPatternMatched(this._onObjectMatched, this);
-    },
-    // "_onObjectMatched" receive an event to access URL & navigation parameters
-    _onObjectMatched: function (oEvent) {
-      // bindElement() creates a object
-      this.getView().bindElement({
-        path:
-          "/" +
-          window.decodeURIComponent(
-            // "arguments" parameter return an object corresponds to the navigation parameters from the route pattern
-            oEvent.getParameter("arguments").invoicePath
-          ),
-        // receive a model for detailed item use
-        model: "invoice",
-      });
-    },
-  });
-});
+sap.ui.define(
+  // add navigation history dependency
+  ["sap/ui/core/mvc/Controller", "sap/ui/core/routing/History"],
+  function (Controller, History) {
+    "use strict";
+    return Controller.extend("sap.ui.demo.walkthrough.controller.Detail", {
+      onInit: function () {
+        // fetch the app router instance
+        var oRouter = this.getOwnerComponent().getRouter();
+        // getRoute() method attach to the "detail" route
+        // attachPatternMatched() method register an internal callback "_onObjectMatched" which will be executed when the route is hit
+        oRouter
+          .getRoute("detail")
+          .attachPatternMatched(this._onObjectMatched, this);
+      },
+      // "_onObjectMatched" receive an event to access URL & navigation parameters
+      _onObjectMatched: function (oEvent) {
+        // bindElement() creates a object
+        this.getView().bindElement({
+          path:
+            "/" +
+            window.decodeURIComponent(
+              // "arguments" parameter return an object corresponds to the navigation parameters from the route pattern
+              oEvent.getParameter("arguments").invoicePath
+            ),
+          // receive a model for detailed item use
+          model: "invoice",
+        });
+      },
+      onNavBack: function () {
+        // access the navigation history & previous hash
+        var oHistory = History.getInstance();
+        var sPreviousHash = oHistory.getPreviousHash();
+        console.log(oHistory);
+        console.log(sPreviousHash);
+
+        if (sPreviousHash !== undefined) {
+          window.history.go(-1);
+        } else {
+          var oRouter = this.getOwnerComponent().getRouter();
+          // second parameter "{}" => do not pass any additional parameters to this route
+          // third parameter "true" => tell the router to replace the current history state with the new one 
+          oRouter.navTo("overview", {}, true);
+        }
+      },
+    });
+  }
+);
