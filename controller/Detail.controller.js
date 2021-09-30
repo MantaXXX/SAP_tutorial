0443f903b9239eadd8d3431c4f091a5d04cc9e85
Jia Lu <pochialu@gmail.com>
2021-09-30 11:44:48 +0800

    feat: add Custom Controls no comment  explain yet

--- controller/Detail.controller.js
+++ controller/Detail.controller.js
@@ -1,7 +1,11 @@
 sap.ui.define(
   // add navigation history dependency
-  ["sap/ui/core/mvc/Controller", "sap/ui/core/routing/History"],
-  function (Controller, History) {
+  [
+    "sap/ui/core/mvc/Controller",
+    "sap/ui/core/routing/History",
+    "sap/m/MessageToast",
+  ],
+  function (Controller, History, MessageToast) {
     "use strict";
     return Controller.extend("sap.ui.demo.walkthrough.controller.Detail", {
       onInit: function () {
@@ -15,6 +19,7 @@ sap.ui.define(
       },
       // "_onObjectMatched" receive an event to access URL & navigation parameters
       _onObjectMatched: function (oEvent) {
+        this.byId("rating").reset();
         // bindElement() creates a object
         this.getView().bindElement({
           path:
@@ -31,17 +36,25 @@ sap.ui.define(
         // access the navigation history & previous hash
         var oHistory = History.getInstance();
         var sPreviousHash = oHistory.getPreviousHash();
-        console.log(oHistory);
-        console.log(sPreviousHash);
 
         if (sPreviousHash !== undefined) {
           window.history.go(-1);
         } else {
           var oRouter = this.getOwnerComponent().getRouter();
           // second parameter "{}" => do not pass any additional parameters to this route
-          // third parameter "true" => tell the router to replace the current history state with the new one 
+          // third parameter "true" => tell the router to replace the current history state with the new one
           oRouter.navTo("overview", {}, true);
         }
+      },
+      onRatingChange: function (oEvent) {
+        var fValue = oEvent.getParameter("value");
+        var oResourceBundle = this.getView()
+          .getModel("i18n")
+          .getResourceBundle();
+
+        MessageToast.show(
+          oResourceBundle.getText("ratingConfirmation", [fValue])
+        );
       },
     });
   }
